a4bee074af1b8e06acb9f488bdb14625e5ffd575
Liz Johnson <lizjohnson@lizs-mbp.lan>
2022-10-22 08:21:54 -0500

    complete analysis updated

--- analysis/utils/cleaning.py
+++ analysis/utils/cleaning.py
@@ -4,9 +4,16 @@ import pandas as pd
 
 
 def lower_case_and_strip_spaces(input: str) -> str:
-    pass
+    return input.lower().strip()
 
 
 def combine_genres_list(input: List[str]) -> str:
-    pass
+    all_genres: List = []
+    for genre_string in input:
+        list_of_genres: List = genre_string.split('|')
+        for genre in list_of_genres:
+            if genre not in all_genres:
+                all_genres.append(genre)
+
+    return '|'.join(all_genres)
 
--- analysis/utils/test_cleaning.py
+++ analysis/utils/test_cleaning.py
@@ -1,12 +1,10 @@
 from typing import List
 
-import pandas as pd
 import pytest
 import os
 
-from analysis.utils.cleaning import lower_case_and_strip_spaces, combine_genres_list, find_duplicates_and_combine
+from analysis.utils.cleaning import lower_case_and_strip_spaces, combine_genres_list
 from analysis.utils.fixtures.movies import expected_movies_dataframe
-from pandas._testing import assert_frame_equal
 
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -22,10 +20,21 @@ DUPLICATE_MOVIE_FIXTURE_PATH = f"{BASE_PATH}/fixtures/duplicated_movie_fixture.c
     ],
 )
 def test_lower_case_and_strip_spaces(input, expected):
-    assert False
+    actual = lower_case_and_strip_spaces(input)
+    assert actual == expected
 
 
+@pytest.mark.parametrize(
+    "genre_string_list, combined",
+    [
+        (["crime|drama|horror", "crime|drama|action"], "crime|drama|horror|action"),
+        (["crime|drama|horror", "fantasy"], "crime|drama|horror|fantasy"),
+        (["horror", "crime|drama|fantasy"], "horror|crime|drama|fantasy"),
+        (["horror", "fantasy", "crime"], "horror|fantasy|crime"),
+    ],
+)
 def test_combine_genres_list(genre_string_list, combined):
-    assert False
+    actual = combine_genres_list(genre_string_list)
+    assert actual == combined
 
 
