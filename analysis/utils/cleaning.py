8d0a1ff1b57d4d83494c803f7c755f56672cb164
Liz Johnson <lizjohnson@lizs-mbp.lan>
2022-10-22 08:32:46 -0500

    reset to initial clean state

--- analysis/utils/cleaning.py
+++ analysis/utils/cleaning.py
@@ -3,17 +3,9 @@ from typing import List, Set
 import pandas as pd
 
 
-def lower_case_and_strip_spaces(input: str) -> str:
-    return input.lower().strip()
+# def lower_case_and_strip_spaces(input: str) -> str:
 
 
-def combine_genres_list(input: List[str]) -> str:
-    all_genres: List = []
-    for genre_string in input:
-        list_of_genres: List = genre_string.split('|')
-        for genre in list_of_genres:
-            if genre not in all_genres:
-                all_genres.append(genre)
+# def combine_genres_list(input: List[str]) -> str:
 
-    return '|'.join(all_genres)
 
--- analysis/utils/test_cleaning.py
+++ analysis/utils/test_cleaning.py
@@ -3,13 +3,6 @@ from typing import List
 import pytest
 import os
 
-from analysis.utils.cleaning import lower_case_and_strip_spaces, combine_genres_list
-from analysis.utils.fixtures.movies import expected_movies_dataframe
-
-
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-DUPLICATE_MOVIE_FIXTURE_PATH = f"{BASE_PATH}/fixtures/duplicated_movie_fixture.csv"
-
 
 @pytest.mark.parametrize(
     "input, expected",
@@ -20,21 +13,10 @@ DUPLICATE_MOVIE_FIXTURE_PATH = f"{BASE_PATH}/fixtures/duplicated_movie_fixture.c
     ],
 )
 def test_lower_case_and_strip_spaces(input, expected):
-    actual = lower_case_and_strip_spaces(input)
-    assert actual == expected
+   assert False
 
 
-@pytest.mark.parametrize(
-    "genre_string_list, combined",
-    [
-        (["crime|drama|horror", "crime|drama|action"], "crime|drama|horror|action"),
-        (["crime|drama|horror", "fantasy"], "crime|drama|horror|fantasy"),
-        (["horror", "crime|drama|fantasy"], "horror|crime|drama|fantasy"),
-        (["horror", "fantasy", "crime"], "horror|fantasy|crime"),
-    ],
-)
 def test_combine_genres_list(genre_string_list, combined):
-    actual = combine_genres_list(genre_string_list)
-    assert actual == combined
+    assert False
 
 
