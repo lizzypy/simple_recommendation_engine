94c36cefc8b7128c934c39f8471c93b99ab981ad
Liz Johnson <lizjohnson@lizs-mbp.lan>
2022-10-23 13:14:16 -0500

    newest clean state

--- analysis/utils/recommendation.py
+++ analysis/utils/recommendation.py
@@ -9,6 +9,6 @@ def get_similar_movies(title: str, similarity_matrix: numpy.ndarray, movies: pd.
     movie_index = indices[title]
     sim_scores = list(enumerate(similarity_matrix[movie_index]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:3]
+    sim_scores = sim_scores[1:31]
     movie_indices = [i[0] for i in sim_scores]
     return list(titles.iloc[movie_indices].values)
